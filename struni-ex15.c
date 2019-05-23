9743d8ef3b21e641fe336bb32c65578333779c3c
Lucas Gonçalves e Alves <48800908+lucasgoncalvesealves@users.noreply.github.com>
2019-05-23 00:37:57 -0300

    Add files via upload

---
+++ struni-ex15.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+
+union Animal {
+	struct Anfibio {
+		int numpatas;
+		int esppele;
+		bool cauda;
+	};
+	struct Ave {
+		int compbico;
+		bool autvoo;
+		int ffreqcanto;
+	};
+	struct Mamifero {
+		int numdentes;
+		int volcerebro;
+		int velterra;
+	};
+	struct Reptil {
+		bool veneno;
+		bool autagua;
+		int espovos;
+	};
+};
+
+void main (void) {
+
+	union Animal humano {
+		struct Mamifero humano {
+			numdentes = 32;
+			volcerebro = 100;
+			velterra = 30;
+		}
+	};
+	
+	union Animal pinguim {
+		struct Ave pinguim {
+			compbico = 20;
+			autvoo = false;
+			ffreqcanto = 22;
+		}
+	};
+			
+	union Animal cobradagua {
+		struct Reptil cobradagua {
+			veneno = true;
+			autagua = true;
+			espovos = 15;
+		}
+	};
+
+	union Animal salamandra {
+		struct Anfibio sapo {
+			numpatas = 4;
+			esppele = 1;
+			cauda = true;
+		}
+	};
+
+	union Animal coyote {
+		struct Mamifero coyote {
+			numdentes = 40;
+			volcerebro = 70;
+			velterra = 50;
+		}
+	};
+
+	union Animal jabuti {
+		struct Reptil jabuti {
+			veneno = false;
+			autagua = false;
+			espovos = 30;
+		}
+	};
+}
