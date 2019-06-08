949cd26be86d4c51cf7157650224557d80c13078
Lucas Gonçalves e Alves <48800908+lucasgoncalvesealves@users.noreply.github.com>
2019-06-07 21:51:18 -0300

    Add files via upload

---
+++ caixas_encadeadas.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+struct Caixa {
+
+	int valor;
+	struct Caixa* prox;
+
+};
+
+void exibe(struct Caixa* caixa) {
+
+	while (caixa != NULL) {
+		printf("%d\n", caixa->valor);
+		caixa = caixa->prox;
+	};
+
+};
+
+void main (void) {
+
+	struct Caixa c1, c2, c3, c4, c5;
+
+	c1.valor = 1;
+	c1.prox = &c2;
+
+	c2.valor = 2;
+	c2.prox = &c3;
+
+	c3.valor = 3;
+	c3.prox = &c4;
+
+	c4.valor = 4;
+	c4.prox = &c5;
+
+	c5.valor = 5;
+	c5.prox = NULL;
+
+	exibe(&c1);
+
+}
